7474782760a927a78527a7337b8616d8e8c1fd43
off1805 <ouafofotsoh@gmail.com>
2026-07-08 23:35:37 +0100

    Initialisation du depot

---
+++ src/main/java/com/example/daeko/DaekoApplication.java
@@ -0,0 +1,13 @@
+package com.example.daeko;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class DaekoApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(DaekoApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/example/daeko/DaekoApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.daeko;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class DaekoApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
